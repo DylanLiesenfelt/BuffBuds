2a96f66718f487095b9c0603b6194bd07548ca25
Dylan Liesenfelt <djliesenfelt@gmail.com>
2025-10-02 15:51:25 -0400

    update production backend

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from app import create_app
 
 # look in config.py for configuration options --> app.config.ClassName to change
-app = create_app('app.config.ProductionConfig')
+app = create_app('app.config.DevelopmentConfig')
 
 if __name__ == "__main__":
     app.run() # debug parameter is set by config setting above
